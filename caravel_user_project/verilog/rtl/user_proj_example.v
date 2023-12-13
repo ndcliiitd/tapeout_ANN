0946e3252ab36da7b0660bdeccbe042651bb4b3a
POOJA BENIWAL <118043668+ndcliiitd@users.noreply.github.com>
2023-12-13 15:05:52 +0530

    Update user_proj_example.v

--- caravel_user_project/verilog/rtl/user_proj_example.v
+++ caravel_user_project/verilog/rtl/user_proj_example.v
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // SPDX-License-Identifier: Apache-2.0
-
+// ndcl
 `default_nettype none
 /*
  *-------------------------------------------------------------
